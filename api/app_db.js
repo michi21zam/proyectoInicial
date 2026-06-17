45859a3f31aeaff9d682ddc9aa05ef7953a85064
Val <valerizambrano16@gmail.com>
2026-06-17 17:12:11 -0600

    The db file name was changed

--- api/app_db.js
+++ api/app_db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'usuarios',
+  database: 'app_db',
   password: 'root123',
   port: 5432
 });
