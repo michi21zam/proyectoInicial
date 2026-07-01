7bd54aaba45f50551db79029bd21207423a0223d
Val <valerizambrano16@gmail.com>
2026-07-01 17:24:43 -0600

    refactor(db): replace hardcoded Knex queries with PostgreSQL functions/procedures

--- api/src/queries/userQueries.js
+++ api/src/queries/userQueries.js
@@ -1,16 +1,32 @@
 const db = require("../../app_db");
 
-const get = () => db('users').select('id', 'name', 'address');
+// SELECT
+const get = async () => {
+    const result = await db.raw("SELECT * FROM fn_get_users()");
+    return result.rows;
+};
 
-const getById = (id) => db('users').select('id', 'name', 'address').where({ id });
+const getById = async (id) => {
+    const result = await db.raw("SELECT * FROM fn_get_user_by_id(?)", [id]);
+    return result.rows;
+};
 
-const checkNameExists = (name) => db('users').select('id', 'name', 'address').where({ name });
+const checkNameExists = async (name) => {
+    const result = await db.raw("SELECT * FROM fn_check_user_name_exists(?)", [name]);
+    return result.rows;
+};
 
-const add = (name, address) => db('users').insert({ name, address });
+// INSERT (returns the new id)
+const add = async (name, address) => {
+    const result = await db.raw("SELECT fn_add_user(?, ?) AS id", [name, address]);
+    return result.rows[0];
+};
 
-const remove = (id) => db('users').where({ id }).del();
+// DELETE / UPDATE
+const remove = (id) => db.raw("CALL sp_remove_user(?)", [id]);
 
-const update = (id, name, address) => db('users').where({ id }).update({ name, address });
+const update = (id, name, address) =>
+    db.raw("CALL sp_update_user(?, ?, ?)", [id, name, address]);
 
 module.exports = {
     get,
